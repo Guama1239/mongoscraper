df3176b52c76f3d871aa96fdb12dc40532b4dc55
Guama1239 <guama1239@gmail.com>
2018-04-07 22:31:57 -0500

    revised code 04072018

--- server.js
+++ server.js
@@ -11,23 +11,22 @@ var cheerio = require("cheerio");
 
 // Require all models
 var db = require("./models");
-var PORT = process.env.PORT || 3000;
-
 // Initialize Express
 var app = express();
+var PORT = process.env.PORT || 3000;
 
 // Configure middleware
 
 // Use morgan logger for logging requests
 app.use(logger("dev"));
 // Use body-parser for handling form submissions
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
-
 // Connect to the Mongo DB
 // Connect to the Mongo DB
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
 
